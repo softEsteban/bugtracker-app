7e857d1742d9837f35cf956d1321a0078ea5ff7a
Esteban <estebantoro.greenman@gmail.com>
2022-11-07 13:04:07 -0500

    navbar separated css styles

--- src/app/landing/content/content.component.ts
+++ src/app/landing/content/content.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
