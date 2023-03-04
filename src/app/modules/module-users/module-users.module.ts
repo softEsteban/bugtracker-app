c1683cc1fd587de0ff9e11d4f526ba999ce87063
Esteban <estebantoro.greenman@gmail.com>
2023-03-03 23:11:28 -0500

    Users and projects modules architecture

---
+++ src/app/modules/module-users/module-users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersListComponent } from './users-list/users-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModuleUsersModule { }
