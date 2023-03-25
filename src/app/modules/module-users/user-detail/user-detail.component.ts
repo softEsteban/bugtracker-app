2189eb044b38face54a934dc2c53af0ef9460327
Esteban <estebantoro.greenman@gmail.com>
2023-03-24 23:36:49 -0500

    Update user form

--- src/app/modules/module-users/user-detail/user-detail.component.ts
+++ src/app/modules/module-users/user-detail/user-detail.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { UsersService } from '../services/users.service';
+
+interface Profile {
+  pro_code: string;
+  pro_name: string;
+}
+
+interface Company {
+  cop_code: string;
+  cop_name: string;
+}
 
 @Component({
   selector: 'app-user-detail',
@@ -14,12 +24,17 @@ export class UserDetailComponent implements OnInit {
   public projectsCount = 0;
   public projects: any = [];
 
+  public profiles: any = [];
+  public companies: any = [];
+
   public userForm = this.formBuilder.group({
     use_code: [''],
     use_name: [''],
     use_lastname: [''],
     use_email: [''],
     use_type: [''],
+    pro_code: [''],
+    cop_code: [''],
   });
 
   userTypes = [
@@ -36,22 +51,28 @@ export class UserDetailComponent implements OnInit {
       text: "Administrator"
     }
   ];
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private usersService: UsersService,
     private formBuilder: FormBuilder) { }
 
+  async ngOnInit(): Promise<void> {
+    await this.getProfilesData();
+    await this.getCompaniesData();
 
-  ngOnInit(): void {
     this.user = history.state.user;
+
     if (this.user && this.user.use_projects) {
       this.projects = this.user.use_projects;
       this.projectsCount = this.projects.length;
 
       this.userForm = this.formBuilder.group({
         use_code: [''],
-        use_name: [''],
-        use_lastname: [''],
-        use_email: [''],
-        use_type: [''],
+        use_name: ['', Validators.required],
+        use_lastname: ['', Validators.required],
+        use_email: [{ value: '', disabled: true }],
+        use_type: ['', Validators.required],
+        pro_code: ['', Validators.required],
+        cop_code: ['', Validators.required],
       });
 
       this.userForm.patchValue({
@@ -59,23 +80,46 @@ export class UserDetailComponent implements OnInit {
         use_name: this.user.use_name,
         use_lastname: this.user.use_lastname,
         use_email: this.user.use_email,
-        use_type: this.user.use_type,
+        pro_code: this.user.pro_code,
+        cop_code: this.user.cop_code,
       });
 
-      //Sets use_type
+      //Sets use_type, pro_code and cop_code
+
       let useTypeControl = this.userForm.get('use_type');
       if (useTypeControl) {
         const userType = this.userTypes.find(type => type.value === this.user.use_type);
         useTypeControl.setValue(userType ? userType.text : null);
       }
 
+      let procodeControl = this.userForm.get('pro_code');
+      if (procodeControl) {
+        const userProfile = this.profiles.find((profile: Profile) => profile.pro_code === this.user.pro_code);
+        procodeControl.setValue(userProfile ? userProfile.pro_code : null);
+      }
 
-
-
-
+      let copcodeControl = this.userForm.get('cop_code');
+      if (copcodeControl) {
+        const userCompany = this.companies.find((company: Company) => company.cop_code === this.user.cop_code);
+        copcodeControl.setValue(userCompany ? userCompany.cop_code : null);
+      }
     }
   }
 
+
+  async getProfilesData() {
+    let data: any = await this.usersService.getAllProfiles();
+    if (data.data.length > 0) {
+      this.profiles = data.data;
+    }
+  }
+
+  async getCompaniesData() {
+    let data: any = await this.usersService.getAllCompanies();
+    if (data.data.length > 0) {
+      this.companies = data.data;
+    }
+  }
 
   triggerEdit() {
     this.editMode = true;
